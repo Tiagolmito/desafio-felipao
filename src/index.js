ad73db8e1b0ddf6e6e4bf5201b5959c8ff0075af
Tiagolmito <tiagoscr15@gmail.com>
2024-04-15 11:49:42 -0300

    projeto finalizado

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-// pra salvar no githut um add. , commit -m "" , git push -u origin main
 let nomePersonagem = "Lorde Darius"
-let xpHeroi = 7.001
+let xpHeroi = 12000
 
+if(xpHeroi >= 0 && xpHeroi <= 1000){
+    console.log("Ferro")
+} if(xpHeroi >= 1001 && xpHeroi <= 2000){
+    console.log("Bronze")
+} if(xpHeroi >= 2001 && xpHeroi <= 5000){
+    console.log("Prata")
+} if(xpHeroi >= 5001 && xpHeroi <= 7000){
+    console.log("Ouro")
+} if(xpHeroi >= 7001 && xpHeroi <= 8000){
+    console.log("Platina")
+} if(xpHeroi >= 8001 && xpHeroi <= 9000){
+    console.log("Ascendente")
+} if(xpHeroi >= 9001 && xpHeroi <= 10000){
+    console.log("Imortal")
+} if(xpHeroi >= 10001){
+    console.log("Radiante")
+} 
 
+console.log(nomePersonagem + " Está no nível de " + xpHeroi)
+    
 
 
 
